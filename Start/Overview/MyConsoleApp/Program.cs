61149c2579fe62a8461c1442b85b086f39d90dbf
Ethan Nunn <nunn_ethan@yahoo.com>
2025-06-27 13:29:43 -0500

    Completed Overview

--- Start/Overview/MyConsoleApp/Program.cs
+++ Start/Overview/MyConsoleApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string response;
+Console.WriteLine("What's your name?");
+response = Console.ReadLine();
+Console.WriteLine($"Enjoy the course, {response}");
+
+// Use the platform to get some information about the system
+OperatingSystem thisOS = Environment.OSVersion;
+Console.WriteLine(thisOS.Platform);
+Console.WriteLine(thisOS.VersionString);
